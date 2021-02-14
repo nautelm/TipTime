b28df6ce20d8b072bd6dcb86db6c50d80235be45
s_elmas <stakasaki707@gmail.com>
2021-02-14 16:38:40 +0900

    changed the app icon

--- app/src/main/java/com/example/stakasaki/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/stakasaki/tiptime/MainActivity.kt
@@ -49,11 +49,8 @@ class MainActivity : AppCompatActivity() {
         if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
-        displayTip(tip)
-    }
-
-    private fun displayTip(tip: Double) {
-        val formattedTip = NumberFormat.getCurrencyInstance().format(0.0)
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
     }
 }
